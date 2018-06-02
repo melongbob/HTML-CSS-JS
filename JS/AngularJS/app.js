8e22106dfe21a49416296fcd74c9acb4c570e2c8
Yun Kim <melongbob@gmail.com>
2018-06-02 14:53:45 -0700

    practice up to end of module 1

--- JS/AngularJS/app.js
+++ JS/AngularJS/app.js
@@ -1,25 +1,64 @@
-(function() {
-  'use strict';
+// (function() {
+//   'use strict';
+//
+//   angular.module('NameCalculator', [])
+//
+//   .controller('NameCalculatorController', function($scope) {
+//     $scope.name = "";
+//     $scope.totalValue = 0;
+//
+//     $scope.displayNumeric = function() {
+//       var totalNameValue = calculateNumericForString($scope.name);
+//       $scope.totalValue = totalNameValue;
+//     };
+//
+//     function calculateNumericForString(string) {
+//       var totalStringValue = 0;
+//       for (var i = 0; i < string.length; i++){
+//         totalStringValue += string.charCodeAt(i);
+//       }
+//
+//       return totalStringValue;
+//     }
+//
+//   });
+// })();
 
-  angular.module('NameCalculator', [])
+// (function () {
+//   'use strict';
+//
+//   angular.module('DIApp', [])
+//   .controller('DIController', DIController);
+//
+//   DIController.$inject = ['$scope', '$filter'];
+//   function DIController ($scope, $filter) {
+//     $scope.name = "SeungYun";
+//
+//     $scope.upper = function () {
+//       var upCase = $filter('uppercase');
+//       $scope.name = upCase($scope.name);
+//     };
+//  }
+// })();
+//
+// (function () {
+//   'use strict';
+//
+//   angular.module('MsgApp', [])
+//   .controller('MsgController', MsgController);
+//
+//   MsgController.$inject = ['$scope'];
+//   function MsgController($scope) {
+//     $scope.name = "SeungYun";
+//     $scope.stateOfBeing = "hungry";
+//     $scope.sayMessage = function () {
+//       return "I am the coolest";
+//     };
+//
+//     $scope.feedKermit = function() {
+//       $scope.stateOfBeing = "happy";
+//     }
+//   }
+// })();
 
-  .controller('NameCalculatorController', function($scope) {
-    $scope.name = "";
-    $scope.totalValue = 0;
-
-    $scope.displayNumeric = function() {
-      var totalNameValue = calculateNumericForString($scope.name);
-      $scope.totalValue = totalNameValue;
-    };
-
-    function calculateNumericForString(string) {
-      var totalStringValue = 0;
-      for (var i = 0; i < string.length; i++){
-        totalStringValue += string.charCodeAt(i);
-      }
-
-      return totalStringValue;
-    }
-
-  });
-})();
+//------------------------END OF MODULE 1
