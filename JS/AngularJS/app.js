50f99870bb2e128f40f1076a62cbc260860226bb
Yun Kim <melongbob@gmail.com>
2018-06-01 23:47:43 -0700

    Name Calculator

--- JS/AngularJS/app.js
+++ JS/AngularJS/app.js
@@ -1,15 +1,25 @@
 (function() {
   'use strict';
 
+  angular.module('NameCalculator', [])
 
+  .controller('NameCalculatorController', function($scope) {
+    $scope.name = "";
+    $scope.totalValue = 0;
 
-  angular.module('myFirstApp', [])
+    $scope.displayNumeric = function() {
+      var totalNameValue = calculateNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
+    };
 
-  .controller('MyFirstController', function($scope) {
-    $scope.name = "Yun";
-    $scope.sayHello = function () {
-      return "Hello Coursera!";
+    function calculateNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++){
+        totalStringValue += string.charCodeAt(i);
+      }
+
+      return totalStringValue;
     }
-  });
 
+  });
 })();
