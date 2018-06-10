ce297798d2128920b5dc51a64cc63e20c119c5c0
Yun Kim <melongbob@gmail.com>
2018-06-09 23:38:18 -0700

    complete ng-repeat

--- JS/AngularJS/app.js
+++ JS/AngularJS/app.js
@@ -40,25 +40,153 @@
 //     };
 //  }
 // })();
-//
+
 // (function () {
 //   'use strict';
 //
 //   angular.module('MsgApp', [])
-//   .controller('MsgController', MsgController);
+//   .controller('MsgController', MsgController)
+//   .filter('loves', LovesFilter)
 //
-//   MsgController.$inject = ['$scope'];
-//   function MsgController($scope) {
+//   MsgController.$inject = ['$scope', 'lovesFilter'];
+//   function MsgController($scope, lovesFilter) {
 //     $scope.name = "SeungYun";
 //     $scope.stateOfBeing = "hungry";
+//     $scope.feedCost = .45;
+//
 //     $scope.sayMessage = function () {
-//       return "I am the coolest";
+//       var msg = "I am the coolest";
+//       // var output = $filter('uppercase')(msg);
+//       // return output;
+//       return msg;
 //     };
 //
-//     $scope.feedKermit = function() {
+//     $scope.feedKermit = function () {
 //       $scope.stateOfBeing = "happy";
-//     }
-//   }
+//     };
+//   }//controller END
+//
 // })();
 
 //------------------------END OF MODULE 1
+
+// (function () {
+// 'use strict';
+//
+// angular.module('MsgApp', [])
+// .controller('MsgController', MsgController)
+// .filter('loves', LovesFilter)
+// .filter('truth', TruthFilter);
+//
+// MsgController.$inject = ['$scope', 'lovesFilter'];
+// function MsgController($scope, lovesFilter) {
+//   $scope.stateOfBeing = "hungry";
+//
+//   $scope.sayMessage = function () {
+//     var msg = "I am the coolest";
+//     return msg;
+//   };
+//
+//   $scope.sayCoolestMessage = function () {
+//     var msg = "I am the coolest";
+//     msg = lovesFilter(msg);
+//     return msg;
+//   };
+//
+//   $scope.feedKermit = function () {
+//     $scope.stateOfBeing = "happy";
+//   };
+// }
+//
+// function LovesFilter() {
+//   return function (input) {
+//     input = input || "";
+//     input = input.replace("coolest", "Kermit");
+//
+//     return input;
+//   };
+// }
+//   function TruthFilter() {
+//     return function(input, target, replace) {
+//       input = input || "";
+//       input = input.replace(target, replace);
+//
+//       return input;
+//     };
+//   }
+//
+// })();
+
+// (function () {
+//   'use strict';
+//
+//   angular.module('CounterApp', [])
+//   .controller('CounterController', CounterController);
+//
+//   CounterController.$inject = ['$scope'];
+//   function CounterController($scope) {
+//     $scope.onceCounter = 0;
+//     $scope.counter = 0;
+//     $scope.name = "SeungYun";
+//
+//     $scope.showNumberOfWatchers = function () {
+//       console.log("# of Watchers: ", $scope.$$watchersCount);
+//     };
+//
+//     $scope.countOnce = function() {
+//       $scope.onceCounter = 1;
+//     };
+//
+//     $scope.upCounter = function() {
+//       $scope.counter++;
+//     }
+//
+//   }
+//
+// })();
+
+(function () {
+  'use strict';
+
+  var shoppingList1 = ["Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter",
+   "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+ ];
+
+ var shoppingList2 = [
+   {
+     name: "Milk",
+     quantity: "2"
+   },
+   {
+     name: "Donuts",
+     quantity: "2"
+   },
+   {
+     name: "Cookies",
+     quantity: "300"
+   },
+   {
+     name: "Chocolate",
+     quantity: "5"
+   }
+ ];
+
+ angular.module('ShoppingListApp', [])
+ .controller('ShoppingListController', ShoppingListController);
+
+ ShoppingListController.$inject = ['$scope'];
+ function ShoppingListController($scope) {
+   $scope.shoppingList1 = shoppingList1;
+   $scope.shoppingList2 = shoppingList2;
+
+   $scope.addToList = function () {
+     var newItem = {
+       name:$scope.newItemName,
+       quantity: $scope.newItemQuantity
+     };
+
+     $scope.shoppingList2.push(newItem);
+   };
+ }
+
+})();
